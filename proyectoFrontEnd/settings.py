2be0196e76aa6fdfbc38e7eb11f510cfeab2988a
SantiagoSR <santiago.santa.ramirez@gmail.com>
2020-05-26 22:03:00 -0500

    adicionando requirements.txt y modificando settings.py

--- proyectoFrontEnd/settings.py
+++ proyectoFrontEnd/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'm*_fm0f1h7@4e1b_=)3iv7s--3yf9bk%k3!q^c9ev=lrwv=xd#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
